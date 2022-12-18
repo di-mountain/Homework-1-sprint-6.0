d38dd28818b66e00c12cac5174e1bb18084363c5
dimountain <gftght@rambler.ru>
2022-12-18 23:08:37 +0300

    Выполнил задачу 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
