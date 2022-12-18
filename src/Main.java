34089d468c009c40f6dd2a04f2dd5a84ba773c20
dimountain <gftght@rambler.ru>
2022-12-18 23:46:50 +0300

    Выполнил задачу 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -14,6 +15,12 @@ public class Main {
     public static void task2() {
         System.out.println("Задача 2");
         for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = 0; i < 17; i+=2) {
             System.out.println(i);
         }
     }
