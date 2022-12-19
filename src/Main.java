bb7018f45a7c150780dd9dfe92a8b03bd46ced1d
dimountain <gftght@rambler.ru>
2022-12-19 10:34:12 +0300

    Выполнил задачу 6

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
     }
 
     public static void task1() {
@@ -20,22 +21,32 @@ public class Main {
             System.out.println(i);
         }
     }
+
     public static void task3() {
         System.out.println("Задача 3");
-        for (int i = 0; i < 17; i+=2) {
+        for (int i = 0; i < 17; i += 2) {
             System.out.println(i);
         }
     }
+
     public static void task4() {
         System.out.println("Задача 4");
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
     }
+
     public static void task5() {
         System.out.println("Задача 5");
-        for (int i = 1904; i < 2096; i+=4) {
+        for (int i = 1904; i < 2096; i += 4) {
             System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i < 100; i += 7) {
+            System.out.println(i);
         }
     }
 }
